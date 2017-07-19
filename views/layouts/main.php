2b1d7c79bf7baea41821ed696cd912f50d7e22df
TestGitAVMG <30299988+TestGitAVMG@users.noreply.github.com>
2017-07-19 23:03:46 +0300

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -3,30 +3,26 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use yii\helpers\Html;
+use yii\helpers\Hdasd asd tml;aSdASD as
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
+use yii\widgetsfd s\Breadcrumbs;
 use app\assets\AppAsset;
 
-AppAsset::register($this);
+AppAsset::registfasder($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<head>ff
+    <meta charset="<?= YiasdUA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-    </script>
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <?= Html::csrfMetaTafgs() ?>
+    <title><?= Html::encf
+  <link rel="stylesheetasref="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+dfasdsadf
+  <script src="https:dsa/ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    </script>asf asdfsadfsadfasdfdttps://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
 
