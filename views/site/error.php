fc91b1baa2e8ce66706f8e316d7ce4a2d042ca7f
TestGitAVMG <30299988+TestGitAVMG@users.noreply.github.com>
2017-07-19 23:06:43 +0300

    Update error.php

--- views/site/error.php
+++ views/site/error.php
@@ -12,16 +12,13 @@ $this->title = $name;
 <div class="site-error">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
-    </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
+f
+    <div class="alert alert-asdf">
+        <?= nl2br(Html::encode($mf
+    <p>asd
+        The above error occurred whfile the Web server was processing your request.
     </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
+    <p>fasdfasd
 
-</div>
+</div>asd
+ff
